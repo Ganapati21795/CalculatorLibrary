958bd032b8d1c6ce0764b49dbd612f5088d9efd1
Ganapati Bhat <ganapati.bhat@thoughtspot.com>
2019-12-18 11:56:45 +0530

    second commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+calculator library to perform math operations
+"""
+
+def add(first_term, second_term):
+	return first_term + second_term
+
+def subtract(first_term, second_term):
+	return first_term - second_term
+
